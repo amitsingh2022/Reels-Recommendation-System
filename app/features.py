02d7c655e0365c87f21f18e1d3c9a62d75f7e7f1
Amit Singh <118271907+amitsingh2022@users.noreply.github.com>
2026-02-05 17:53:25 +0545

    Updated app

--- app/features.py
+++ app/features.py
@@ -1,1 +1,20 @@
-# here will be the features code
+def build_user_features(df):
+    user_features = df.groupby("user_id").agg({
+        "watch_time": "mean",
+        "liked": "sum",
+        "shared": "sum",
+        "commented": "sum"
+    }).reset_index()
+
+    return user_features
+
+def build_reel_features(df):
+    reel_features = df.groupby("reel_id").agg({
+        "watch_time": "mean",
+        "liked": "sum",
+        "shared": "sum",
+        "commented": "sum"
+    }).reset_index()
+
+    return reel_features
+
