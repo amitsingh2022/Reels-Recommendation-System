df40ae48193147a681f9ad25a6f107085fa3b23e
Amit Singh <118271907+amitsingh2022@users.noreply.github.com>
2026-02-05 17:44:45 +0545

    Updating dataset.py

--- training/dataset.py
+++ training/dataset.py
@@ -1,1 +1,28 @@
-# here will be the dataset portion
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+class ReelsDataset(Dataset):
+    def __init__(self, csv_path):
+        self.df = pd.read_csv(csv_path)
+
+        # interaction score (label)
+        self.df["label"] = (
+            0.2 * (self.df["watch_time"] > 3).astype(int) +
+            1.0 * self.df["liked"] +
+            2.0 * self.df["shared"] +
+            3.0 * self.df["commented"]
+        )
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+
+        return {
+            "user_id": torch.tensor(row.user_id),
+            "reel_id": torch.tensor(row.reel_id),
+            "watch_time": torch.tensor(row.watch_time, dtype=torch.float),
+            "label": torch.tensor(row.label, dtype=torch.float),
+        }
