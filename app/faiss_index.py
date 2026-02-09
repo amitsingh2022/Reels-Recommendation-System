a8369bc12ab961d70555ca137aab3200f5dc9639
Amit Singh <118271907+amitsingh2022@users.noreply.github.com>
2026-02-09 22:31:23 +0545

    feat Added faiss index)

--- app/faiss_index.py
+++ app/faiss_index.py
@@ -1,1 +1,25 @@
-# here will be the faiss_index code
+import faiss
+import numpy as np
+
+
+class FaissRetriever:
+    def __init__(self, reel_embeddings_path):
+        self.reel_embeddings = np.load(reel_embeddings_path).astype("float32")
+
+        self.embedding_dim = self.reel_embeddings.shape[1]
+
+        # Cosine similarity → normalize vectors
+        faiss.normalize_L2(self.reel_embeddings)
+
+        self.index = faiss.IndexFlatIP(self.embedding_dim)
+        self.index.add(self.reel_embeddings)
+
+        print("✅ FAISS index built")
+
+    def search(self, user_embedding, k=10):
+        user_embedding = user_embedding.astype("float32").reshape(1, -1)
+        faiss.normalize_L2(user_embedding)
+
+        scores, indices = self.index.search(user_embedding, k)
+
+        return indices[0], scores[0]
